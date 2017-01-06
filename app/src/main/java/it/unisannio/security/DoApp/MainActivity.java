b82e1897aa6e2e49ee53f023355c8c56e48963f8
antonyflour <antoniofarina1702@gmail.com>
2017-01-06 20:01:15 +0100

    Funzionante con 2 MalIntent inviati all'app com.project.antonyflour.intentfilterex

--- app/src/main/java/it/unisannio/security/DoApp/MainActivity.java
+++ app/src/main/java/it/unisannio/security/DoApp/MainActivity.java
@@ -6,39 +6,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //@luigi observa!
+        getSU();
     }
 
     public void fuzzerService(View v) throws FileNotFoundException, InterruptedException {
 
-       /* Intent service = new Intent(this, LogIntentService.class);
-        startService(service);*/
+        Intent i = new Intent(this, FuzzerService.class);
 
-        Intent analize = new Intent(this, FuzzerService.class);
-        startService(analize);
-
-        Intent i = new Intent();
-
-
-        //TODO: inserire il componente da una lista
-        ComponentName cn;
-        cn = new ComponentName("com.project.antonyflour.intentfilterex",
-                "com.project.antonyflour.intentfilterex.ShareActivity");
-
-
-        i.setComponent(cn);
-        startActivity(i);
+        //TODO: sta roba deve venire da una lista di scelta dell'app
+        i.putExtra("pkgname", "com.project.antonyflour.intentfilterex");
+        startService(i);
 
     }
 
     public void goAppList (View v){
         Intent i = new Intent(this, AppListActivity.class);
         startActivity(i);
+    }
+
+    private java.lang.Process getSU(){
+        java.lang.Process suProcess = null;
+        try {
+            suProcess = Runtime.getRuntime().exec("su");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return suProcess;
     }
 
 
